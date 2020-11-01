9894e9249712dc0592c285a881a8ea7098458bab
athenscats <athenscatsgr@gmail.com>
2020-11-01 15:18:09 +0200

    Added parent to skills admin

--- flamepoint/resume/admin.py
+++ flamepoint/resume/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import SkillCategory,Skill,Experience, Responsibilities,MainProfile
 
-admin.site.register(SkillCategory)
+
 admin.site.register(Skill)
 admin.site.register(MainProfile)
+
+class SkillCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title','parent')
+
+
+
+admin.site.register(SkillCategory,SkillCategoryAdmin)
 
 class ResponsibilitiesAdmin(admin.TabularInline):
     model = Responsibilities
