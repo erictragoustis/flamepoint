d7d473f2eb7bf097846e210df2867a8dd6c0c659
athenscats <athenscatsgr@gmail.com>
2020-11-01 15:21:33 +0200

    Added Category and skill% to Skill Admin

--- flamepoint/resume/admin.py
+++ flamepoint/resume/admin.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from .models import SkillCategory,Skill,Experience, Responsibilities,MainProfile
 
 
-admin.site.register(Skill)
+
 admin.site.register(MainProfile)
 
 class SkillCategoryAdmin(admin.ModelAdmin):
     list_display = ('title','parent')
 
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('title','category','skill')
 
 
+admin.site.register(Skill,SkillAdmin)
 admin.site.register(SkillCategory,SkillCategoryAdmin)
 
 class ResponsibilitiesAdmin(admin.TabularInline):
